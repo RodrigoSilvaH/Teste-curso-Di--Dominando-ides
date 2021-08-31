4acdbd5f05c6d81e62d9e6b8f86448066bde4b32
85-rod <rod-igo@hotmail.com>
2021-08-31 15:21:14 -0300

    Teste ides

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,13 +1,25 @@
 package br.com.dio;
 
+import br.com.dio.model.gatos;
+
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		int a = 2;
+		
+		gatos gato = new gatos();
+		Livros Livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(Livros);
+		
+		/*int a = 2;
 		int b = 3;
-		System.out.println("hello World " + (a+b));
+		System.out.println("hello World " + (a+b));*/
 	}
 
 	}
 
-
+class Livros{
+	private String nome;
+	private String npag;
+}
